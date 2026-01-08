1b90dab8f1af29d0df5e465ea95bba1d259ca714
Athirson Santos <athirson07@gmail.com>
2026-01-07 22:15:24 -0300

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Obrigado pelo mangá do Vagabond Bebel!! ☆*: .｡. o(≧▽≦)o .｡.:*☆</h1>;
+  return (
+    <h1>Obrigado pelo mangá do Vagabond Bebel!! ☆*: .｡. o(≧▽≦)o .｡.:*☆</h1>
+  );
 }
 
 export default Home;
